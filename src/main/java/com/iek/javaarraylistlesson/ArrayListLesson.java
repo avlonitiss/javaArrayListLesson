a9b95a32a4c2e5d4f2f9328c319458061938d260
Spyros <Spyros@Markos.mshome.net>
2022-10-23 15:24:01 +0300

    Fifth ArrayList Lesson -   Αναζήτηση στοιχείων λίστας

--- src/main/java/com/iek/javaarraylistlesson/ArrayListLesson.java
+++ src/main/java/com/iek/javaarraylistlesson/ArrayListLesson.java
@@ -7,6 +7,7 @@ package com.iek.javaarraylistlesson;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -34,11 +35,34 @@ public class ArrayListLesson {
             complexArray.add(Arrays.asList("Elenh",15,32.20));
             //εδώ προστέθηκαν μία εγγραφή με ένα data type σε μία εγγραφή
             
+
+            //Τροποποίηση εκτύπωσης με απόκρυψη βάρους
+            System.out.println("Name,  Age");
             for (List myRecords : complexArray) {
                 System.out.println(
-                    myRecords.get(0)+" "+myRecords.get(1)+" "+myRecords.get(2));
+                    myRecords.get(0)+" "+myRecords.get(1));
             }
             
+            //Αναζήτηση βάρους με Input το όνομα
+            Scanner myKeyboard = new Scanner(System.in);
+            
+            System.out.println("Enter name to find weight");
+            String name=myKeyboard.nextLine();
+            
+            System.out.println("I'm searchnig "+name+" weight");
+            
+            
+            complexArray.forEach(arrayName->{
+                
+                if(arrayName.get(0).equals(name)){
+                    System.out.println("I found the person...");
+                    System.out.println(name+" is weighting "+arrayName.get(2)+" Kg");
+                    System.exit(0);
+                }else {
+                    System.out.println("This person isn't found yet");
+                }
+            
+            });
             
   
     }
