553ec3c61728dcfe704b1bf8a1d29b6ae1e604d6
Spyros <Spyros@Markos.mshome.net>
2022-10-23 14:21:29 +0300

    Third ArrayList Lesson - προσθεση στοιχείων διαφορετικών data type και διαφορετικών τύπων Προσπέλαση θέσεων που υπάρχουν (μόνο η πρώτη θέση είναι κοινή)

--- src/main/java/com/iek/javaarraylistlesson/ArrayListLesson.java
+++ src/main/java/com/iek/javaarraylistlesson/ArrayListLesson.java
@@ -5,6 +5,7 @@
 package com.iek.javaarraylistlesson;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,6 +20,26 @@ public class ArrayListLesson {
     public static void main(String[] args) {
         // TODO code application logic here
             ArrayList<List> complexArray = new ArrayList<>();
+                
+            // προσθήκη εγγραφών στοιχείων διαφορετικών data types στη λίστα
+            
+            complexArray.add(Arrays.asList("Costas",15,34.12));
+            //εδώ προστέθηκε μια εγγραφή με τρία διαφορετικά data types σε μία εγγραφή
+            //(String, int, double)
+            
+            complexArray.add(Arrays.asList("Giannis",850.32,14));
+              //εδώ προστέθηκε μια εγγγραφή με τρία διαφορετικά data types σε μία εγγραφή
+            //(String, double, int) διαφιρετικά από την προηγούμενη εγγραφή
+            
+            complexArray.add(Arrays.asList(420));
+            //εδώ προστέθηκαν μία εγγραφή με ένα data type σε μία εγγραφή
+            
+            for (List myRecords : complexArray) {
+                System.out.println(
+                    myRecords.get(0));
+            }
+            
+            
   
     }
     
